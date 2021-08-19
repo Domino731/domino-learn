0162d57b080fb6ce08f5050e05dd6987b1e929dd
Domino731 <d.orzechowski18@gmail.com>
2021-08-19 13:18:29 +0200

    improvements - taskValidationJS function-

--- src/functions/taskValidationJS.ts
+++ src/functions/taskValidationJS.ts
@@ -1,13 +1,11 @@
 import {IFJsTaskTargets} from "../types/types";
 
 export const taskValidationJS = (consoleTextArr: any[] ,taskTarget: IFJsTaskTargets,  addPoints: () => void) => {
-    consoleTextArr.map(el => {
-           if(el === taskTarget.console){
-               addPoints();
-               return taskTarget.solved = true;
-           }
-           else{
-               return taskTarget.solved = false;
-           }
-    });
+    if(consoleTextArr.includes(taskTarget.console)){
+        addPoints();
+        return taskTarget.solved = true;
+    }
+    else {
+        return taskTarget.solved = false;
+    }
 };
