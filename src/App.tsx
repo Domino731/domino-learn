55a53c7663c31f2f25a726630000eb7b0bb40a4a
Domino731 <d.orzechowski18@gmail.com>
2021-08-22 11:18:53 +0200

    improvements - RouterScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ import {JsTask} from "./components/jsTask/JsTask";
 import {QuizMenu} from "./components/quiz/QuizMenu";
 import {CodeEditor} from "./components/CodeEditor/CodeEditor";
 import {Quiz} from "./components/quiz/Quiz";
-
+import {RouterScrollToTop} from "./components/other/RouterScrollToTop";
 function App() {
     return (
         <Router>
+            <RouterScrollToTop/>
             <Route exact path="/" component={HomePage}/>
             <Route path="/html-task/:taskNumber" component={HtmlTask}/>
             <Route path="/css-task/:taskNumber" component={CssTask}/>
